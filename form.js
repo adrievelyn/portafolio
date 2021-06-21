d2cc4d95f5c14c2f2134e8f810500117d5d910b9
adrievelyn <adrievelyn@gmail.com>
2021-06-20 20:35:34 -0400

    agregando formulario

---
+++ form.js
@@ -0,0 +1,12 @@
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#sendMsg')
+
+    $form.addEventListener('submit', handleSubmit)
+
+    function handleSubmit(event){
+
+        event.preventDefault()
+        const form = new FormData(this)
+        $buttonMailto.setAttribute('href', `mailto:adrievelyn@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+        $buttonMailto.click()
+    }
